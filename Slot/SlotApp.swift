6fd47eb9b4550e05e06a70aa43ce82c8ecfae119
Kato Steven Mubiru <kato.steven60@gmail.com>
2022-12-28 05:47:57 +0300

    Initial Commit

---
+++ Slot/SlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotApp.swift
+//  Slot
+//
+//  Created by Kato Steven Mubiru on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
